fde350e702313f43f83cda96acc01cf3a57675ec
Mr-Molotov <castrejonjoshua99@gmail.com>
2025-07-31 16:26:54 -0500

    ex1

--- app/src/main/java/com/example/potatofinal/actividades/DetailActivity.java
+++ app/src/main/java/com/example/potatofinal/actividades/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.potatofinal.actividades;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +9,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.potatofinal.R;
 import com.example.potatofinal.databinding.ActivityDetailBinding;
+import com.example.potatofinal.dominios.Item;
 
 public class DetailActivity extends AppCompatActivity {
     ActivityDetailBinding binding;
+    private Item object;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detail);
+        binding=ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        getIntentExtra();
+        setVariable();
+
+        }
+
+    private void setVariable() {
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$"+object.getPrice());
+        binding.backBtn.setOnClickListener(v -> finish());
+        binding.bedTxt.setText(""+object.getBed());
+        binding.durationTxt.setText(object.getDuration());
+        binding.distanceTxt.setText(object.getDistance());
+        binding.descriptionTxt.setText(object.getDescription());
+        binding.addressTxt.setText(object.getAddress());
+        binding.ratingBar2.setRating((float)object.getScore());
+        binding.ratingTxt.setText(object.getScore()+"Reseñas");
+
+        Glide.with(DetailActivity.this)
+                .load(object.getPic())
+                .into(binding.pic);
+
+        binding.addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
+    }
+
+    private void getIntentExtra() {
+        object=(Item) getIntent().getSerializableExtra("object");
+
+    });
     }
 }
